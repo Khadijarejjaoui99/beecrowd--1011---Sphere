eb863e0ad161b664e90f5612068bc1c91035a113
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 21:15:39 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,15 @@
+// Solution of 1011 - Sphere with Bonus in beecrowd
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+  double R, pi = 3.14159, VOLUME;
+  cin >> R;
+  VOLUME = (4 / 3.0) * pi * R * R * R;
+  cout << "VOLUME = " << fixed << setprecision (3) << VOLUME << endl;
+    system("pause");
+    return 0;
+}
